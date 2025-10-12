a46b5a4c23710ba9b3c06b0cca0a89e00f56bfe3
JOEY HIGGINS <sparty1758@gmail.com>
2025-10-12 01:42:40 -0400

    updated to use RMSE

--- src/mod_lstm_0.py
+++ src/mod_lstm_0.py
@@ -3,18 +3,15 @@ import matplotlib.pyplot as plt
 import torch.nn as nn
 import pandas as pd
 import numpy as np
-import warnings
 import torch
 
 # Importing partial packages
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
 from sklearn.preprocessing import MinMaxScaler
 from utility import create_lag, load_data
 from torch.autograd import Variable
 from tqdm import tqdm
-
-# Ignoring warnings
-warnings.filterwarnings("ignore")
 
 # Defining the LSTM model
 class LSTM(nn.Module):
@@ -49,60 +46,76 @@ class LSTM(nn.Module):
 
 
 if __name__ == "__main__":
+
 	# Importing data
     neighbor_groups, df_anchor_neighor, df_log_return_pca = load_data()
 
-    # Running LSTM for each anchor-neighbor pair
-    for i, j in tqdm(zip(df_anchor_neighor["anchor"], df_anchor_neighor["neighbor"]), total=len(df_anchor_neighor)):
+    # Compiling scores
+    final = []
 
-        # Preparing data for train test split
-        df_1 = df_log_return_pca[[i, j]]
-        df_1.dropna(inplace=True)
-        df_1 = create_lag(j, df_1).drop(columns=[i])
-        
-        # Time-based train-test split
-        df_1["target"] = df_1[j].shift(-1)
-        
-        # Define features (X) and target (y)
-        features = [col for col in df_1.columns if col == j or col.startswith("MA_") or col.startswith("return_lag_")]
-        X = df_1[features]
-        y = df_1["target"]
+    # Running LR for each anchor neighbor pair
+    for option in ["Training_with_Anchor", "Training_without_Anchor"]:
 
-        # Splitting training data
-        train_size = int(len(X) * 0.8)
-        train_X = Variable(torch.Tensor(np.array(X[0:train_size]))).unsqueeze(2) # https://stackoverflow.com/questions/57237352/what-does-unsqueeze-do-in-pytorch
-        train_y = Variable(torch.Tensor(np.array(y[0:train_size])))
-        
-        # Splitting testing data
-        test_size = len(y) - train_size
-        test_X = Variable(torch.Tensor(np.array(X[train_size:len(X)]))).unsqueeze(2) # https://stackoverflow.com/questions/57237352/what-does-unsqueeze-do-in-pytorch
-        test_y = Variable(torch.Tensor(np.array(y[train_size:len(y)])))
+        # Running LSTM for each anchor-neighbor pair
+        for i, j in tqdm(zip(df_anchor_neighor["anchor"], df_anchor_neighor["neighbor"]), total=len(df_anchor_neighor)):
 
-        num_epochs = 2000
-        learning_rate = 0.01
-        input_size = 1
-        hidden_size = 2
-        num_layers = 1
-        num_classes = 1
-        seq_length = len(train_X)
+            # Initializing dictionary to hold scores
+            scores = {}
 
-        lstm = LSTM(num_classes, input_size, hidden_size, num_layers)
+            # Preparing data for train test split
+            df_1 = df_log_return_pca[[i, j]]
+            df_1 = df_1.drop(columns=[i])
+            df_1 = create_lag(j, df_1)
+            
+            # Time-based train-test split
+            df_1["target"] = df_1[j].shift(-1)
+            df_1.dropna(inplace=True)
 
-        criterion = torch.nn.MSELoss()  # mean-squared error for regression
-        optimizer = torch.optim.Adam(lstm.parameters(), lr=learning_rate)
-        # optimizer = torch.optim.SGD(lstm.parameters(), lr=learning_rate)
+            # Define features (X) and target (y)
+            if option == "Training_without_Anchor":
+                features = [col for col in df_1.columns]
+                i = None
+            else:
+                features = [col for col in df_1.columns if col == j or col.startswith("MA_") or col.startswith("return_lag_")]
+                
+            X = df_1[features]
+            y = df_1["target"]
 
-        # Train the model
-        for epoch in tqdm(range(num_epochs)):
-            outputs = lstm(train_X)
-            optimizer.zero_grad()
+            # Splitting training data
+            train_size = int(len(X) * 0.8)
+            train_X = Variable(torch.Tensor(np.array(X[0:train_size]))).unsqueeze(2) # https://stackoverflow.com/questions/57237352/what-does-unsqueeze-do-in-pytorch
+            train_y = Variable(torch.Tensor(np.array(y[0:train_size])))
+            
+            # Splitting testing data
+            test_size = len(y) - train_size
+            test_X = Variable(torch.Tensor(np.array(X[train_size:len(X)]))).unsqueeze(2) # https://stackoverflow.com/questions/57237352/what-does-unsqueeze-do-in-pytorch
+            test_y = Variable(torch.Tensor(np.array(y[train_size:len(y)])))
 
-            # obtain the loss function
-            loss = criterion(
-                outputs, train_y
-            )
-            loss.backward()
+            num_epochs = 2000
+            learning_rate = 0.01
+            input_size = 1
+            hidden_size = 2
+            num_layers = 1
+            num_classes = 1
+            seq_length = len(train_X)
 
-            optimizer.step()
-            if epoch % 100 == 0:
-                print("Epoch: %d, loss: %1.5f" % (epoch, loss.item()))
+            lstm = LSTM(num_classes, input_size, hidden_size, num_layers)
+
+            criterion = torch.nn.MSELoss()  # mean-squared error for regression
+            optimizer = torch.optim.Adam(lstm.parameters(), lr=learning_rate)
+            # optimizer = torch.optim.SGD(lstm.parameters(), lr=learning_rate)
+
+            # Train the model
+            for epoch in tqdm(range(num_epochs)):
+                outputs = lstm(train_X)
+                optimizer.zero_grad()
+
+                # obtain the loss function
+                loss = torch.sqrt(criterion(
+                    outputs, train_y
+                ))
+                loss.backward()
+
+                optimizer.step()
+                if epoch % 100 == 0:
+                    print("Epoch: %d, loss: %1.5f" % (epoch, loss.item()))
